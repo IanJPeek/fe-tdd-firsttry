e9f62876c503f9f141f132d83ce294566017bc78
IanJPeek <IanJPeek@gmail.com>
2020-02-10 16:16:11 +0000

    will run 2 sample tests for basic 'Counter' Component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
+// import { render } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("UNIT Testing the App component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  describe("Component validation", () => {
+    it("displays 0 as a default value", () => {
+      expect(wrapper.find("h1").text()).toContain("0");
+    });
+  });
+
+  it("should increase counter when the button is clicked", () => {
+    wrapper.find("button").simulate("click");
+    expect(wrapper.find("h1").text()).toContain("1");
+  });
 });
